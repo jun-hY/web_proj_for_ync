c80b6833c461dbde0f49941cba0d566684e91383
jun-hy <dlvm789@gmail.com>
2024-11-13 16:10:56 +0900

    refactor: html 변경 부분 수정

--- html/script/weather.js
+++ html/script/weather.js
@@ -32,145 +32,35 @@ const Time = Number(minutes) >= 45 ? `${hours}30` : `${Number(hours) - 1}30`;
 const ServiceKey = `8pZx3zpwmiP6xng2EUvTlOz6qnesip%2BuYn70GCdXph%2FQek0Ws9N6r0YU4iLHZgputh87KbB8m6XsQGecpxiIaA%3D%3D`;
 const CurrnetWeather = `http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey=${ServiceKey}&numOfRows=100&pageNo=1&dataType=JSON&base_date=${Today}&base_time=${Time}&nx=89&ny=90`
 
-console.log(CurrnetWeather)
-
 fetch(CurrnetWeather).then(res => res.json().then(data => {
-    var CurrentTemp = 0;
-    var CurrentHumidity = 0;
-    var CurrentWind = 0;
+    const CurrentTemp = document.querySelector(".current-weather .temperature");
+    const CurrentHumidity = document.querySelector(".weather-details .humidity-value");
+    const CurrentWind = document.querySelector(".weather-details .wind-value");
+    const CurrentSkyIcon = document.querySelector(".current-weather .icon");
+    const CurrentSkyDescription = document.querySelector(".current-weather .description");
 
     const CurrentWeatherInfo = data.response.body.items.item;
+    // Object.keys(CurrentWeatherInfo).forEach((i) => {
+    //     console.log(CurrentWeatherInfo[i])
+    // });
     Object.keys(CurrentWeatherInfo).forEach((i) => {
-        console.log(CurrentWeatherInfo[i])
+        const isCurrentTemp = CurrentWeatherInfo[i].category == `T1H` && CurrentWeatherInfo[i].fcstTime == `${Number(hours) + 1}00`
+        const isCurrentHumidity = CurrentWeatherInfo[i].category == `REH` && CurrentWeatherInfo[i].fcstTime == `${Number(hours) + 1}00`
+        const isCurrentWind = CurrentWeatherInfo[i].category == `WSD` && CurrentWeatherInfo[i].fcstTime == `${Number(hours) + 1}00`
+        const isCurrentSky = CurrentWeatherInfo[i].category == `SKY` && CurrentWeatherInfo[i].fcstTime == `${Number(hours) + 1}00`
+        if (isCurrentTemp) {
+            CurrentTemp.innerText = `${CurrentWeatherInfo[i].fcstValue}°C`;
+        }
+        if (isCurrentHumidity) {
+            CurrentHumidity.innerText = `${CurrentWeatherInfo[i].fcstValue}%`;
+        }
+        if (isCurrentWind) {
+            CurrentWind.innerText = `${CurrentWeatherInfo[i].fcstValue} m/s`;
+        }
+        if (isCurrentSky) {
+            CurrentWeatherInfo[i].fcstValue;
+            CurrentSkyIcon.innerText = `${CurrentWeatherInfo[i].fcstValue < 3 ? `☀️` : CurrentWeatherInfo[i].fcstValue < 4 ? `🌤️` : `☁️`}`;
+            CurrentSkyDescription.innerText = `${CurrentWeatherInfo[i].fcstValue < 3 ? `맑음` : CurrentWeatherInfo[i].fcstValue < 4 ? `구름많음` : `흐림`}`;
+        }
     });
-    Object.keys(CurrentWeatherInfo).forEach((i) => {
-        if (CurrentWeatherInfo[i].category == `T1H`) {
-            CurrentTemp = CurrentWeatherInfo[i].obsrValue;
-        }
-        if (CurrentWeatherInfo[i].category == `REH`) {
-            CurrentHumidity = CurrentWeatherInfo[i].obsrValue;
-        }
-        if (CurrentWeatherInfo[i].category == `WSD`) {
-            CurrentWind = CurrentWeatherInfo[i].obsrValue;
-        }
-    });
-    WeatherCard.innerHTML = `
-        <div class="header">
-            <h1 id="location">Daegu, TAE</h1>
-        </div>
-
-        <div class="current-weather">
-            <div class="temperature">${CurrentTemp}°C</div>
-            <div class="condition">
-                <div class="icon">🌤️</div>
-                <div class="description">Partly Cloudy</div>
-            </div>
-        </div>
-
-        <div class="weather-details">
-            <div class="detail">
-                <i data-lucide="droplets"></i>
-                <div class="label">Humidity</div>
-                <div class="value">${CurrentHumidity}%</div>
-            </div>
-            <div class="detail">
-                <i data-lucide="wind"></i>
-                <div class="label">Wind</div>
-                <div class="value">${CurrentWind} m/s</div>
-            </div>
-            <div class="detail">
-                <i data-lucide="thermometer"></i>
-                <div class="label">Pressure</div>
-                <div class="value">1015 hPa</div>
-            </div>
-        </div>
-
-        <div class="forecast">
-            <div class="day">
-                <div class="name">Mon</div>
-                <div class="icon">☀️</div>
-                <div class="temp">75°F</div>
-            </div>
-            <div class="day">
-                <div class="name">Tue</div>
-                <div class="icon">🌤️</div>
-                <div class="temp">73°F</div>
-            </div>
-            <div class="day">
-                <div class="name">Wed</div>
-                <div class="icon">🌧️</div>
-                <div class="temp">68°F</div>
-            </div>
-            <div class="day">
-                <div class="name">Thu</div>
-                <div class="icon">⛅</div>
-                <div class="temp">70°F</div>
-            </div>
-            <div class="day">
-                <div class="name">Fri</div>
-                <div class="icon">☀️</div>
-                <div class="temp">72°F</div>
-            </div>
-        </div>`;
 }));
-
-// setTimeout(() => {
-//     WeatherCard.innerHTML = `
-//     <div class="header">
-//                 <h1 id="location">New York, NY</h1>
-//             </div>
-
-//             <div class="current-weather">
-//                 <div class="temperature">${CurrentTemp}°C</div>
-//                 <div class="condition">
-//                     <div class="icon">🌤️</div>
-//                     <div class="description">Partly Cloudy</div>
-//                 </div>
-//             </div>
-
-//             <div class="weather-details">
-//                 <div class="detail">
-//                     <i data-lucide="droplets"></i>
-//                     <div class="label">Humidity</div>
-//                     <div class="value">${CurrentHumidity}%</div>
-//                 </div>
-//                 <div class="detail">
-//                     <i data-lucide="wind"></i>
-//                     <div class="label">Wind</div>
-//                     <div class="value">${CurrentWind} m/s</div>
-//                 </div>
-//                 <div class="detail">
-//                     <i data-lucide="thermometer"></i>
-//                     <div class="label">Pressure</div>
-//                     <div class="value">1015 hPa</div>
-//                 </div>
-//             </div>
-
-//             <div class="forecast">
-//                 <div class="day">
-//                     <div class="name">Mon</div>
-//                     <div class="icon">☀️</div>
-//                     <div class="temp">75°F</div>
-//                 </div>
-//                 <div class="day">
-//                     <div class="name">Tue</div>
-//                     <div class="icon">🌤️</div>
-//                     <div class="temp">73°F</div>
-//                 </div>
-//                 <div class="day">
-//                     <div class="name">Wed</div>
-//                     <div class="icon">🌧️</div>
-//                     <div class="temp">68°F</div>
-//                 </div>
-//                 <div class="day">
-//                     <div class="name">Thu</div>
-//                     <div class="icon">⛅</div>
-//                     <div class="temp">70°F</div>
-//                 </div>
-//                 <div class="day">
-//                     <div class="name">Fri</div>
-//                     <div class="icon">☀️</div>
-//                     <div class="temp">72°F</div>
-//                 </div>
-//             </div>`;
-// }, 3000);
