cb4c8166e13f80a364400726e3be79d0318edb2e
jun-hy <dlvm789@gmail.com>
2024-11-15 17:08:59 +0900

    style: 상수 코드 클래스 생성

---
+++ html/script/init_constant.js
@@ -0,0 +1,29 @@
+export class Names {
+    constructor() {
+        this.FIRST_DAY_NAME = document.querySelector('#first-day .name');
+        this.SECOND_DAY_NAME = document.querySelector('#second-day .name');
+        this.THIRD_DAY_NAME = document.querySelector('#third-day .name');
+        this.FOURTH_DAY_NAME = document.querySelector('#fourth-day .name');
+        this.FIFTH_DAY_NAME = document.querySelector('#fifth-day .name');
+    }
+}
+
+export class Icons {
+    constructor() {
+        this.FIRST_DAY_ICON = document.querySelector('#first-day .icon');
+        this.SECOND_DAY_ICON = document.querySelector('#second-day .icon');
+        this.THIRD_DAY_ICON = document.querySelector('#third-day .icon');
+        this.FOURTH_DAY_ICON = document.querySelector('#fourth-day .icon');
+        this.FIFTH_DAY_ICON = document.querySelector('#fifth-day .icon');
+    }
+}
+
+export class Temps {
+    constructor() {
+        this.FIRST_DAY_TEMP = document.querySelector('#first-day .temp');
+        this.SECOND_DAY_TEMP = document.querySelector('#second-day .temp');
+        this.THIRD_DAY_TEMP = document.querySelector('#third-day .temp');
+        this.FOURTH_DAY_TEMP = document.querySelector('#fourth-day .temp');
+        this.FIFTH_DAY_TEMP = document.querySelector('#fifth-day .temp');
+    }
+}
